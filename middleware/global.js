74608cca48c45091d76faa59563026826c434289
Alex Merced <willcodeforfood@alexmerced.com>
2020-09-11 08:49:51 -0400

    bodyparser

--- middleware/global.js
+++ middleware/global.js
@@ -1,15 +1,17 @@
 //////////////////////////////////////
 // Global Middleware
 //////////////////////////////////////
-const {Middler} = require('am-merver')
-const middler = new Middler()
+const { Middler } = require("am-merver");
+const middler = new Middler();
 
 //REGISTER MIDDLEWARE
 
 //basic logger
 middler.addMiddleware((req, res) => {
-    console.log(`${req.method} request sent to ${req.url} `)
-})
+  console.log(`${req.method} request sent to ${req.url} `);
+});
+//bodyparser
+middler.addMiddleware(middler.bodyParser);
 
 //Export Middler
-module.exports = middler
+module.exports = middler;
